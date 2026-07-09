89f7dd72298f4d360b8b6c4219960ea0d5ac05f3
TonYVanilka <vanyavahryhin@gmail.com>
2026-07-09 08:36:21 +0000

    delete memoryArena

--- RixtenEngine/tests/main.cpp
+++ RixtenEngine/tests/main.cpp
@@ -1,7 +1,22 @@
 #include <cstdio>
+#include "core/memory/DArr.h"
+
+struct vec1 {
+    int x;
+    ~vec1() {
+        printf("destructor vec1 have called\n");
+    }
+};
 
 int main() {
 
-    printf("-----This is testing build of engine-----");
-    
+    printf("-----This is testing build of engine-----\n");
+
+    DArr<vec1> pool(10);
+
+    pool.push_back({16110});
+
+    pool.push_back({15404});
+    printf("pool[0] = %d\n", pool[0].x);
+
 }
